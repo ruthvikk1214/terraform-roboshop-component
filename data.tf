f80cbfc54113dffaa55f4f4f49476b905ab66ba2
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-24 03:59:49 +0530

    changed to hyphens for listener arns

--- data.tf
+++ data.tf
@@ -26,10 +26,10 @@ data "aws_ssm_parameter" "sg_id" {
   name = "/${var.project}/${var.environment}/${var.component}_sg_id"
 }
 data "aws_ssm_parameter" "backend_alb_listener_arn" {
-  name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
+  name = "/${var.project}/${var.environment}/backend-alb_listener_arn"
 }
 data "aws_ssm_parameter" "frontend_alb_listener_arn" {
-  name = "/${var.project}/${var.environment}/frontend_alb_listener_arn"
+  name = "/${var.project}/${var.environment}/frontend-alb_listener_arn"
 }
 data "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project}/${var.environment}/vpc_id"
