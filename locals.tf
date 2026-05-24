dadc449a47eeca3cad28a64a95d73889431868b9
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-24 21:22:38 +0530

    updated component

--- locals.tf
+++ locals.tf
@@ -6,7 +6,7 @@ locals {
   sg_id                     = data.aws_ssm_parameter.sg_id.value
   alb_listener_arn          = var.component == "frontend" ? data.aws_ssm_parameter.frontend_alb_listener_arn.value : data.aws_ssm_parameter.backend_alb_listener_arn.value
   port_number               = var.component == "frontend" ? "80" : "8080"
-  host_header               = var.component == "frontend" ? "${var.component}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
+  host_header = var.component == "frontend" ? "${var.component}.frontend-alb-roboshop-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
   health_check_path         = var.component == "frontend" ? "/" : "/health"
   private_subnet_ids        = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
   common_tags = {
